663ac2f617fc6715596315d0f6583741e288ea8b
haukeeckel <Haukeeckel@posteo.de>
2021-11-22 13:05:25 +0100

    User Profile Character

--- routes/Character.routes.js
+++ routes/Character.routes.js
@@ -1,24 +1,71 @@
 const router = require("express").Router();
 const Character = require("../models/Character.model");
 // const User = require("../models/User.model");
-// const Adventure = require("../models/Adventure.model");
+const Adventure = require("../models/Adventure.model");
 
 /* ------ Authorization ------ */
 
 /* ------ Browse ------ */
 router.get("/character/:characterId", async (req, res, next) => {
-  let loggedIn = false;
-  if (req.session.keks) {
-    loggedIn = true;
-  }
+  const loggedIn = !!req.session.keks;
   const { characterId } = req.params;
 
   try {
     const character = await Character.findById({ _id: characterId }).populate(
       "userId"
     );
+
     const { userId: user } = character;
-    res.render("character/adventureDetails", { character, loggedIn, user });
+
+    res.render("character/profileInfo", { character, loggedIn, user });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/character/:characterId/adventure", async (req, res, next) => {
+  const loggedIn = !!req.session.keks;
+  const { characterId } = req.params;
+
+  try {
+    const character = await Character.findById(characterId).populate("userId");
+    const user = character.userId;
+    const adventure = await Adventure.findOne({
+      participantIds: characterId,
+    })
+      .populate("userIds")
+      .populate("gameMasterId");
+
+    res.render("character/profileAdventure", {
+      character,
+      adventure,
+      user,
+      loggedIn,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/character/:characterId/member", async (req, res, next) => {
+  const loggedIn = !!req.session.keks;
+  const { characterId } = req.params;
+
+  try {
+    const character = await Character.findById(characterId).populate("userId");
+    const user = character.userId;
+    const adventure = await Adventure.findOne({
+      participantIds: characterId,
+    })
+      .populate("participantIds")
+      .populate("gameMasterId");
+    console.log(adventure);
+    res.render("character/profileMember", {
+      character,
+      adventure,
+      user,
+      loggedIn,
+    });
   } catch (err) {
     next(err);
   }
